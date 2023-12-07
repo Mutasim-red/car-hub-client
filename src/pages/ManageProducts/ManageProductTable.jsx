35b805f5f8a979c679bac81181369ecbe2995da0
Mutasim-red <mutasimhossain3@gmail.com>
2023-12-07 18:27:10 +0600

    delete single vehicle completed.

--- src/pages/ManageProducts/ManageProductTable.jsx
+++ src/pages/ManageProducts/ManageProductTable.jsx
@@ -1,9 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 function ManageProductsTable() {
-  const vehicles = useLoaderData();
-  console.log(vehicles);
+  //get all the fetched data
+  const data = useLoaderData();
+  const [vehicles,setVehicles] = useState(data);
+  //function for handling the delete vehicle
+  function handleDeleteVehicle(vehicleId) {
+    fetch(`http://localhost:3000/delete-by-id/${vehicleId}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) =>{
+        if(data.deletedCount>0){
+         const otherVehicle = vehicles?.filter(vehicle => vehicle._id != vehicleId)
+        setVehicles(otherVehicle)
+        }
+      });
+  }
 
   return (
     <div className="overflow-x-auto">
@@ -26,9 +40,13 @@ function ManageProductsTable() {
                 <Link to={`/vehicle/${vehicle?._id}`}>
                   <button className="btn btn-success">D</button>
                 </Link>
-                <button className="btn btn-warning">X</button>
+                <button
+                  className="btn btn-warning"
+                  onClick={() => handleDeleteVehicle(vehicle._id)}
+                >
+                  X
+                </button>
                 <Link to={`/update-vehicle/${vehicle?._id}`}>
-                  {" "}
                   <button className="btn btn-outline">U</button>
                 </Link>
               </td>
--- src/pages/UpdateProduct/UpdateProductForm.jsx
+++ src/pages/UpdateProduct/UpdateProductForm.jsx
@@ -1,9 +1,9 @@
 import { useLoaderData, useNavigate } from "react-router-dom";
 
-function UpdateProductForm(){
-    const vehicle =useLoaderData();
-    const navigate = useNavigate();
-    function handleUpdateVehicle(event){
+function UpdateProductForm() {
+  const vehicle = useLoaderData();
+  const navigate = useNavigate();
+  function handleUpdateVehicle(event) {
     event.preventDefault();
     const make = event.target.make.value;
     const model = event.target.model.value;
@@ -19,63 +19,64 @@ function UpdateProductForm(){
     };
     //Now make a PUT request to the server from updating this vehicleobject
     console.log(updatedvehicle);
-    fetch(`http://localhost:3000/update-by-id/${vehicle._id}`,{
-    method:"PUT",
-    Headers:{
-        "Content-type": "application/json"
-    },
-    body: JSON.stringify(updatedvehicle)
-})
-.then(res=> res.json())
-.then(data=> {
-    if(data.modifiedCount==1){
-        navigate(`/manage-products`)
-    }
-}) 
-}
-    return(
-<form
-         onSubmit={handleUpdateVehicle}
-        className="my-10 flex flex-col gap-y-4"
-      >
-        <input
-          type="text"
-          placeholder="Car Company"
-          defaultValue={vehicle?.make}
-          name="make"
-          className="input w-full input-bordered"
-        />
-        <input
-          type="text"
-          placeholder="Car model"
-          defaultValue={vehicle?.model}
-          name="model"
-          className="input w-full input-bordered"
-        />
-        <input
-          type="text"
-          placeholder="Car image"
-          defaultValue={vehicle?.image}
-          name="image"
-          className="input w-full input-bordered"
-        />
-        <input
-          type="text"
-          placeholder="Car rent($ per hour)"
-          name="rent"
-          defaultValue={vehicle?.rent}
-          className="input w-full input-bordered"
-        />
-        <textarea
-          className="textarea textarea-bordered w-full"
-          name="Description"
-          defaultValue={vehicle?.Description}
-          placeholder="Vehicle Description"
-        ></textarea>
-        <div className="flex justify-center">
-          <input type="submit" className="btn btn-primary"></input>
-        </div>
-      </form>
-    )
+    fetch(`http://localhost:3000/update-by-id/${vehicle._id}`, {
+      method:"PUT",
+      Headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedvehicle),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount == 1) {
+          navigate(`/manage-products`);
+        }
+      });
+  }
+  return (
+    <form
+      method="PUT"
+      onSubmit={handleUpdateVehicle}
+      className="my-10 flex flex-col gap-y-4"
+    >
+      <input
+        type="text"
+        placeholder="Car Company"
+        defaultValue={vehicle?.make}
+        name="make"
+        className="input w-full input-bordered"
+      />
+      <input
+        type="text"
+        placeholder="Car model"
+        defaultValue={vehicle?.model}
+        name="model"
+        className="input w-full input-bordered"
+      />
+      <input
+        type="text"
+        placeholder="Car image"
+        defaultValue={vehicle?.image}
+        name="image"
+        className="input w-full input-bordered"
+      />
+      <input
+        type="text"
+        placeholder="Car rent($ per hour)"
+        name="rent"
+        defaultValue={vehicle?.rent}
+        className="input w-full input-bordered"
+      />
+      <textarea
+        className="textarea textarea-bordered w-full"
+        name="Description"
+        defaultValue={vehicle?.Description}
+        placeholder="Vehicle Description"
+      ></textarea>
+      <div className="flex justify-center">
+        <input type="submit" className="btn btn-primary"></input>
+      </div>
+    </form>
+  );
 }
 export default UpdateProductForm;
